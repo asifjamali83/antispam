7d74f1f32022a5d018353c613c568add2badbbae
Shah Zain <45283590+asifjamali83@users.noreply.github.com>
2019-09-22 20:14:18 +0500

    Update reject.py

--- reject.py
+++ reject.py
@@ -3,13 +3,13 @@
 from LineAPI.linepy import *
 from LineAPI.akad.ttypes import Message
 from LineAPI.akad.ttypes import ContentType as Type
-from LineAPI.akad.ttypes import ChatRoomAnnouncementC>
+#from LineAPI.akad.ttypes import ChatRoomAnnouncementC>
 from LineAPI.akad.ttypes import ChatRoomAnnouncement
 from datetime import datetime, timedelta
 from time import sleep
 from bs4 import BeautifulSoup
-from humanfriendly import format_timespan, format_siz>
-import time, random, multiprocessing, sys, json, code>
+#from humanfriendly import format_timespan, format_siz>
+#import time, random, multiprocessing, sys, json, code>
 from gtts import gTTS
 from googletrans import Translator
 
@@ -22,4 +22,4 @@ else:
         print("Reject " + dz.getGroup(groups).name)
         sleep(0.7)
         dz.rejectGroupInvitation(groups)
-    print("\nYou reject" + str(int1) + "groups invita>
+#    print("\nYou reject" + str(int1) + "groups invita>
