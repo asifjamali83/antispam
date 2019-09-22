9528af63f0f6a40a412fd58cb3ad468a91b511fc
Shah Zain <45283590+asifjamali83@users.noreply.github.com>
2019-09-22 19:25:04 +0500

    Update reject.py

--- reject.py
+++ reject.py
@@ -1,15 +1,25 @@
-# BY TEAMBOTMAX
-# -*- coding: utf-8 -*-
-from LineAPI import *
+#THAKS TO @shahzain786
+
+from LineAPI.linepy import *
+from LineAPI.akad.ttypes import Message
+from LineAPI.akad.ttypes import ContentType as Type
+from LineAPI.akad.ttypes import ChatRoomAnnouncementC>
+from LineAPI.akad.ttypes import ChatRoomAnnouncement
+from datetime import datetime, timedelta
 from time import sleep
-maxbots = LINE()
-int1 = len(maxbots.getGroupIdsInvited())
+from bs4 import BeautifulSoup
+from humanfriendly import format_timespan, format_siz>
+import time, random, multiprocessing, sys, json, code>
+from gtts import gTTS
+from googletrans import Translator
+
+dz = LINE("")
+int1 = len(dz.getGroupIdsInvited())
 if int1 == 0:
     print("No groups inviting")
 else:
-    for groups in maxbots.getGroupIdsInvited():
-        print("Reject " + maxbots.getGroup(groups).name)
+    for groups in dz.getGroupIdsInvited():
+        print("Reject " + dz.getGroup(groups).name)
         sleep(0.7)
-        maxbots.rejectGroupInvitation(groups)
-    print("\nYou reject" + str(int1) + "groups invitation")
-    
+        dz.rejectGroupInvitation(groups)
+    print("\nYou reject" + str(int1) + "groups invita>
